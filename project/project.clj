9cbca1d64e51c45d12c85414fa92a8d75336b090
sanja.kostic <Sanja@DESKTOP-2TNQAP6>
2017-03-19 13:56:21 +0100

    First version

--- project/project.clj
+++ project/project.clj
@@ -1,6 +1,22 @@
-(defproject project "0.1.0-SNAPSHOT"
+(defproject project "0.8.13-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [hiccup-table "0.2.0"]
+                 [ring-server "0.3.1"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 ;[org.xerial/sqlite-jdbc "3.7.2"]
+                 [mysql/mysql-connector-java "5.1.18"]]
+  :plugins [[lein-ring "0.8.12"]]
+  :ring {:handler pharmacy.handler/app
+         :init pharmacy.handler/init
+         :destroy pharmacy.handler/destroy}
+  :profiles
+  {:uberjar {:aot :all}
+   :production
+   {:ring
+    {:open-browser? true, :stacktraces? true, :auto-reload? false}}
+   :dev
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.3.1"]]}})
